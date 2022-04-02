afe7cff5686198fb02632bc24ac32ed89ea6d7ab
bec <ryyng95@gmail.com>
2022-04-02 06:12:57 -0400

    Add user login and JWT fetching

--- react-ui/src/views/landing.jsx
+++ react-ui/src/views/landing.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 const bcrypt = require('bcryptjs');
 
 const LandingPage = () => {
+
+	const [userData, setUserData] = useState(null);
+	const [token, setToken] = useState('')
 
 	const navigateTo = useNavigate();
 
@@ -10,17 +14,28 @@ const LandingPage = () => {
 		const form = e.target.form;
 		const user = {
 			"username": form.username.value,
-			"password": "",
+			"password": form.password.value,
 			"create": false
 		}
+		console.log("login userinfo: " + user);
 		let response;
-		bcrypt.genSalt(10, async function(err, salt) {
-			bcrypt.hash("B4c0/\/", salt, async function(err, hash) {
-				user.password = hash;
-				await callAPI("POST", user)
-			});
-		});
-		navigateTo("/projects")
+		let data = await callAPI("POST", user)
+		await comparePasswords(form.password.value, userData, user.username)
+	}
+
+	const comparePasswords = async (localPassword, data, username) => {
+		bcrypt.compare(localPassword, data, async (err, isMatch) => {
+			if(isMatch){
+				await callAPI("POST", {
+					"username": username,
+					"password": data,
+					"create": false
+				}, true);
+				console.log("JWT token: " + token);
+			} else if (!isMatch) {
+				console.log("Bad Password")
+			}
+		})
 	}
 
 	const handleSignUp = async e => {
@@ -33,16 +48,16 @@ const LandingPage = () => {
 		}
 		let response;
 		bcrypt.genSalt(10, async function(err, salt) {
-			bcrypt.hash("B4c0/\/", salt, async function(err, hash) {
+			bcrypt.hash(form.password.value, salt, async function(err, hash) {
 				user.password = hash;
 				await callAPI("POST", user)
 			});
 		});
-		navigateTo("/projects")
 	}
 
 
-	const callAPI = async (method, body) => {
+	const callAPI = async (method, body, token = false) => {
+		console.log("API Call Body: " + body)
 		const requestOptions = {
 			method: method,
 			headers: { 'Content-Type': 'application/json' },
@@ -53,9 +68,17 @@ const LandingPage = () => {
 			await fetch(API_URL, requestOptions)
 				.then(res => res.json())
 				.then((data) => {
-					console.log(data)
+					console.log("API Call Return: " + data)
+					if(body.create){
+						alert(data.body);
+					}else if(token){
+						setToken(data.body);
+						navigateTo("/projects");
+					}
+					else{
+						setUserData(data.body);
+					}
 				})
-				.catch(console.log)
 		}catch(e){
 			console.log(e);
 		}
@@ -101,7 +124,7 @@ const LandingPage = () => {
 						<form>
 							<label for="chk" aria-hidden="true">Login</label>
 							<input type="text" name="username" placeholder="Username" required="" />
-							<input type="password" name="pswd" placeholder="Password" required="" />
+							<input type="password" name="password" placeholder="Password" required="" />
 							<button onClick={handleLogin} className="landingButton">Login</button>
 						</form>
 					</div>
