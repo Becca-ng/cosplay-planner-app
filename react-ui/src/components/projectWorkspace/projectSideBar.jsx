3727c6002feeece72a89b3d7c9e230f4b13ae290
Kevin Hu <kevin.hu@ncino.com>
2022-04-05 13:20:39 -0400

    feat: add in logic for calling aws API

--- react-ui/src/components/projectWorkspace/projectSideBar.jsx
+++ react-ui/src/components/projectWorkspace/projectSideBar.jsx
@@ -5,22 +5,47 @@ import { logoTest, plusIcon } from "../../images/imageIndex";
 const ProjectSideBar = ({projects, setProjects, setCurrentProject}) => {
 
     const [newProjectName, setNewProjectName] = useState('');
-  
-    //You'd replace this URL with the one from the API Gateway
+
     useEffect(() => {
+      console.log(sessionStorage.getItem('user'));
+      console.log(sessionStorage.getItem('token'));
+        
+      const body = {
+        'username' : sessionStorage.getItem('user')
+      }
+
+      // const returnedProjects = await callAPI("POST", body);
+
       fetch('http://localhost:8080/api/projects')
-      .then(res => res.json())
-      .then(data => setProjects(data));
+        .then(res => res.json())
+        .then(data => setProjects(data));
     }, []);
+
+    const callAPI = async (method, body) => {
+      console.log("API Call Body: " + body)
+      const requestOptions = {
+        method: method,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      };
+
+      console.log(requestOptions);
+      console.log(PROJECTS_API_URL)
+  
+      try{
+        await fetch(PROJECTS_API_URL, requestOptions)
+          .then(res => res.json())
+          .then((data) => {
+            setProjects(data.body.Items);
+          })
+      }catch(e){
+        console.log("error" + e);
+        throw e;
+      }
+    }
   
     const handleChange = e => {
       setNewProjectName(e.target.value);
-    };
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({newProjectName})
     };
   
     const handleCreate = e => {
@@ -41,6 +66,7 @@ const ProjectSideBar = ({projects, setProjects, setCurrentProject}) => {
         //This will need to call to the API to delete
         console.log(e.target)
         console.log(e.target.attributes.index.value)
+        console.log(projects[e.target.attributes.index.value])
 
         //Take the existing list of projects and remove the project from it with splice. We can access the index by doing e.target.index or something (it's set on the html button element)
         //Then do setProjects to the new projects with the item removed
@@ -110,3 +136,46 @@ const ProjectSideBar = ({projects, setProjects, setCurrentProject}) => {
 }
 
 export default ProjectSideBar;
+
+//https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+const PROJECTS_API_URL = 'https://fast-tor-11029.herokuapp.com/https://3uck5y4t5g.execute-api.us-east-1.amazonaws.com/live/projects';
+
+    /**
+      POST to the Projects endpoint takes in
+        body: {
+          username: "a username"
+        }
+
+        return: {
+          statusCode: 200/400,
+          body: {
+            Items: [a List of Projects]
+          }
+        }
+
+      DELETE to the Projects endpoint takes in
+        body: {
+          username: "a username",
+          index: 3
+        }
+
+        return: {
+          statusCode: 200/400,
+          body: "Project successfully deleted!"
+        }
+
+      PATCH to the Projects endpoint takes in
+        body: {
+          username: "a username",
+          index: 4,
+          project: {
+            name: "a name"
+            ...
+          }
+        }
+
+        return: {
+          statusCode: 200/400,
+          body: "Project updated successfully!"
+        }
+    */
