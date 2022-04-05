024d263986ac18daae440d1059e546e3e9574961
bec <ryyng95@gmail.com>
2022-04-05 17:15:14 -0400

    css

--- react-ui/src/images/imageIndex.jsx
+++ react-ui/src/images/imageIndex.jsx
@@ -1,6 +1,8 @@
 import logoTest from "./logo/logoTest.png";
 import plusIcon from "./icons/plusIcon.png";
-import loginBackground from "./background/loginBackground.png"
+import loginBackground from "./background/loginBackground.png";
+import checkIcon from "./icons/checkIcon.png";
+import editIcon from "./icons/editIcon.png"
+import xIcon from "./icons/x.png"
 
-
-export { logoTest , plusIcon, loginBackground};
+export { logoTest , plusIcon, loginBackground , checkIcon , editIcon , xIcon };
